96d53d2d51853430def1c2466abca1f5a2fc51a1
Shresth Singh <118938064+Shresth941@users.noreply.github.com>
2024-10-20 18:46:50 +0530

    Update weatherservice.jsx

--- frontend/src/Service/weatherservice.jsx
+++ frontend/src/Service/weatherservice.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/weather/summary';
+const API_URL = 'https://weather11-szqt.onrender.com/api/weather/summary';
 
 export const getWeatherSummary = async (city = '') => {
   try {
