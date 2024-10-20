68f0b863c36d737765c56b2e3e12289644d452b2
Shresth941 <1rn20ec087.shresthsingh@gmail.com>
2024-10-20 17:11:57 +0530

    commit

--- frontend/src/Service/weatherservice.jsx
+++ frontend/src/Service/weatherservice.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/weather/summary';
+const API_URL = 'https://weather2-w56f.onrender.com';
 
 export const getWeatherSummary = async (city = '') => {
   try {
